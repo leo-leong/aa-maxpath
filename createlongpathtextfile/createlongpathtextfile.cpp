13a0ec16647932d12f1c63d3d48f044bd05be946
Leo Leong <leoleo-31650@outlook.com>
2023-03-09 17:56:54 -0800

    fixed path corruption resulting in creating file within solution folder and non-ASCII characters

--- createlongpathtextfile/createlongpathtextfile.cpp
+++ createlongpathtextfile/createlongpathtextfile.cpp
@@ -14,19 +14,38 @@ int main()
     using namespace std;
 
     string longPath{ 0 };
+    size_t size{ 0 };
+    size_t requiredSize{ 0 };
+    wchar_t* pwc;
 
     cout << "Please enter a long full path name in the UNC format. E.g. \\\\?\\C:\\Temp\\verylongfilename.txt):\n";
     getline(std::cin, longPath);
 
-    //cout << "\nYou've entered:\n";
-    //cout << longPath;
+    auto cstr = longPath.c_str();
+    requiredSize = strlen(cstr)+1;
+    pwc = new wchar_t[requiredSize];
+    mbstowcs_s(&size, pwc, requiredSize, cstr, requiredSize - 1);
 
     //TODO: Bug on creating file within solution folder instead of intended target location
-    HANDLE fileHandle = CreateFileW((LPCWCHAR)longPath.c_str(), GENERIC_WRITE, NULL, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+     HANDLE fileHandle = CreateFileW(pwc, GENERIC_WRITE, NULL, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
      if (INVALID_HANDLE_VALUE == fileHandle)
     {
-        cerr << "\n\nFailed to create file!" << endl;
-        return 1;
+		LPVOID lpMsgBuf;
+		DWORD dw = GetLastError();
+
+		FormatMessage(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			dw,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0, NULL);
+
+        _tprintf(TEXT("\n\nFailed to create file with error:\n\n\t%s"), (LPTSTR)lpMsgBuf);
+
+         return 1;
     }
 
      DWORD dwRetVal{ 0 };
@@ -34,7 +53,7 @@ int main()
      TCHAR Path[BUFSIZE];
      dwRetVal = GetFinalPathNameByHandleW(fileHandle, Path, BUFSIZE, VOLUME_NAME_NT);
      if (dwRetVal < BUFSIZE)
-     {
+     { 
          _tprintf(TEXT("\nThe final path is: \n%s\n"), Path);
      }
 
